72625c14d234c61188c810b6f0e8ca45e58c0961
Emilien Petiteau <emilien.petiteau@telecomnancy.eu>
2021-05-09 18:14:08 +0200

    une petite upgrade

--- application/index.py
+++ application/index.py
@@ -30,11 +30,11 @@ def results():
 
 
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+# def get_db():
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db = g._database = sqlite3.connect(DATABASE)
+#     return db
 
 @app.teardown_appcontext
 def close_connection(exeption):
