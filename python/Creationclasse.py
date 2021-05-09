96cba9bd440672a00f3ab6a20b51a7ee4eae93ed
BALDUZ Thomas <thomas.balduz@telecomnancy.eu>
2021-05-09 22:14:33 +0200

    +dico

--- python/Creationclasse.py
+++ python/Creationclasse.py
@@ -1,13 +1,33 @@
 import pandas as pd
 import os
+from pathlib import Path
+from os.path import basename
+
+folder_path = "C:\P2I"
+
+dico = {}
+
+for dirpath, dirnames, filenames in os.walk(folder_path):
+  for filename in filenames:
+      print(type(filename))
+      FilePrefix, FileExtension = os.path.splitext(filename)
+      if (FileExtension == ".xlsx"):
+          dico["{0}".format(FilePrefix)] = pd.read_excel(folder_path+"\\"+basename(filename))
+print(dico)
+          
+          
+
 
 #Création de la table VILLE
-
+"""
 Inscription = pd.read_excel("C:\P2I\Inscription.xlsx")
 Ville = pd.DataFrame()
 Ville['CP']=Inscription['Unnamed: 14']
 Ville['nom_ville'] = Inscription['Unnamed: 15']
 Ville.to_excel('Ville.xlsx',index=False)
+
+L=Ville.columns
+print(L[1])
 
 
 #Création de la table CANDIDAT
@@ -27,5 +47,7 @@ Candidat["code_pays_nationalite"] = Inscription['Unnamed: 10']
 Candidat["csp_pere"] = Inscription['Unnamed: 45']
 Candidat["arrondissement_naissance"] = Inscription['Unnamed: 51']
 Candidat["qualite"] = Inscription['Unnamed: 53']
-Candidat.to_excel('Candidat.xlsx',index=False)
+Candidat.to_excel('Candidat.xlsx',index=False)"""
+
+#Creation de la table CONCOURS
 
