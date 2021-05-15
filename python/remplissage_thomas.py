0d78a54b88f28993e1336174638eeb6aac7b7524
BALDUZ Thomas <thomas.balduz@telecomnancy.eu>
2021-05-15 13:24:29 +0200

    version bugue

--- python/remplissage_thomas.py
+++ python/remplissage_thomas.py
@@ -1,3 +1,4 @@
+from numpy import NAN, NaN, nan
 import pandas as pd
 import os
 from pathlib import Path
@@ -8,8 +9,8 @@ from flask import Flask, Blueprint, render_template, abort, request, redirect
 from flask import g
 from collections import OrderedDict
 
-folder_path = "C:\\Users\\thoma\Documents\\TN\P2I\\project-grp12\\dow\\data\\public"
-filepath_inscription = "C:\\Users\\thoma\Documents\\TN\P2I\\project-grp12\\dow\\data\\public\\Inscription.xlsx"
+folder_path = "..\dow-master\data\public"
+filepath_inscription = "..\dow-master\data\public\Inscription.xlsx"
 app = Flask(__name__)
 
 def select_filiere(st): #selectionne la filière dans le nom d'un fichier
@@ -70,36 +71,59 @@ with app.app_context():
 
     print('Remplissage table : Admission')
 
-    nb_filiere = 11
+    c.execute("DELETE FROM admissions;") 
+    df = pd.read_excel(filepath_inscription,header=1)
+    req = "INSERT INTO admissions (Can_cod, admissible, admis) VALUES "
+    tab = df.to_numpy()
+    
+    dic = {}
+    for row in tab:
+        dic[row[0]] = [row[0]]
+        dic[row[0]].append(nan)
+        dic[row[0]].append(nan)
+    
+    df = dico["ADMISSIBLE_MP-SPE"]
+    tab = df.to_numpy()
+    for row in tab:
+        dic[row[0]][1]=row[0]
+    df = dico["ADMIS_MP-SPE"]
+    tab = df.to_numpy()
+    for row in tab:
+        dic[row[0]][2]=row[12]
 
+    i = len(dic)
+    print(i)
+    for x in dic:
+        req += f"(\"{x}\",  \"{dic[x][1]}\", \"{dic[x][2]})\""
+        i -= 1
+        if i > 0: req += ", "
+    req += ";"
+
+    # for dirpath, dirnames, filenames in os.walk(folder_path):
+    #     for filename in filenames:
+    #         FilePrefix, FileExtension = os.path.splitext(filename)
+    #         if ("ADMISSIBLE" in FilePrefix) and ("SPE" in FilePrefix) and ("ADMISSIBLE_ATS" not in FilePrefix) and ("ADMISSIBLE_PC" not in FilePrefix): 
+    #             df = dico['ADMISSIBLE_'+select_filiere(FilePrefix)]
+    #             tab = df.to_numpy()
+    #             for row in tab:
+    #                 dic[row[0]][1]=row[0]
+
+            # if ("ADMIS_" in FilePrefix) and ("SPE" in FilePrefix) and ("ADMIS_ATS" not in FilePrefix) and ("ADMIS_PC" not in FilePrefix):
+            #     df = dico['ADMIS_'+select_filiere(FilePrefix)]
+            #     tab = df.to_numpy()
+            #     for row in tab:
+            #         dic[row[0]][2]=row[12]
+                    #dic[row[0]].append(row[12])
+     # 3 pb : le premier dans filiere PC,un can_cod introuvable dans Inscription (44232 je crois) +2e pb : longueur de liste variante dans dic + sqlite3.OperationalError: parser stack overflow
     
 
-    for dirpath, dirnames, filenames in os.walk(folder_path):
-        for filename in filenames:
-            FilePrefix, FileExtension = os.path.splitext(filename)
-            print('ADMIS_'+select_filiere(FilePrefix))
-            if "ADMISSIBLE" in FilePrefix: 
-                df = dico['ADMISSIBLE_'+select_filiere(FilePrefix)]
-                rows_can_cod= []
-                for index, row in df.iterrows():
-                    rows_can_cod.append([row['Can _cod']])
-                c.executemany("INSERT OR IGNORE INTO admissions (Can_cod) VALUES (?)", rows_can_cod)
-                c.execute("COMMIT;")
+    #c.executemany("INSERT OR IGNORE INTO admissions (Can_cod, admissible, admis) VALUES (?, ?, ?)", dic)
+    c.execute(req)
+    c.execute("COMMIT;")
 
     print('Table remplie')
 
 
 
-
-def born_stop(L):
-    aux = []
-    R = []
-    for i in L:
-        if i =='STOP':
-            R.append(aux)
-            aux = []
-            continue
-        aux.append(i)
-    return(R)
 
 
