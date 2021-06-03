186da3cf03b7f54b9976d7e8112de910831f2148
Emilien Petiteau <emilien.petiteau@telecomnancy.eu>
2021-06-03 15:25:48 +0200

    chart 3+4

--- application/static/js/charts.js
+++ application/static/js/charts.js
@@ -1,4 +1,5 @@
-function charts(chartInfo1, chartInfo2) {
+function charts(chartInfo1, chartInfo2, chartInfo3, chartInfo4) {
+
     const ctx1 = document.getElementById('chart1').getContext('2d');
     var myChart = new Chart(ctx1, {
         type: 'bar',
@@ -85,6 +86,109 @@ function charts(chartInfo1, chartInfo2) {
                 legend: {
                     display: false
                 }
+            }
+        }
+    });
+
+    const ctx3 = document.getElementById('chart3').getContext('2d');
+    var myChart = new Chart(ctx3, {
+        type: 'bar',
+        data: {
+            labels: chartInfo3["label"],
+            datasets: [{
+                data: chartInfo3["data"],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Candidats admissibles par nationalité',
+                    font: {
+                        size: 20
+                    }
+                },
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                y: {
+                    suggestedMin: 0,
+                    suggestedMax: 5000
+                },
+                xAxes: [{
+                    display: false
+                }]
+            }
+        }
+    });
+
+    const ctx4 = document.getElementById('chart4').getContext('2d');
+    var myChart = new Chart(ctx4, {
+        type: 'bar',
+        data: {
+            labels: chartInfo4["label"],
+            datasets: [{
+                data: chartInfo4["data"],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Candidats admis par nationalité',
+                    font: {
+                        size: 20
+                    }
+                },
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                y: {
+                    suggestedMin: 0,
+                    suggestedMax: 5000
+                },
             }
         }
     });
