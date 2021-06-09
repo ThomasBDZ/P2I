f3b5cdb281f05533648c77b3c985f9e901204944
Emilien Petiteau <emilien.petiteau@telecomnancy.eu>
2021-06-09 23:05:46 +0200

    launch_app.sh update

--- launch_app.sh
+++ launch_app.sh
@@ -2,13 +2,8 @@
 
 FILE="./application/env"
 
-if test -f $FILE; then
-    echo "env directory already exists"
-else
-    echo "creating env directory"
-    python3 -m venv $FILE
-fi
-
+echo "creating env directory"
+python3 -m venv $FILE
 source $FILE/bin/activate
 pip3 install Flask
 export FLASK_APP="./application/app.py"
