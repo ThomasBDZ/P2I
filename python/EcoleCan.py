369b6a5ffddbe505b97c5ed3cb80bfa6b5afab2a
BALDUZ Thomas <thomas.balduz@telecomnancy.eu>
2021-06-13 20:20:41 +0200

    Ajout colonne n-demi + total

--- python/EcoleCan.py
+++ python/EcoleCan.py
@@ -1,3 +1,4 @@
+from typing import final
 import pandas as pd
 import os
 from pathlib import Path
@@ -28,10 +29,11 @@ for dirpath, dirnames, filenames in os.walk(folder_path):
         if ((FileExtension == ".xlsx") and ("CMT_spe_XXXX" in FilePrefix)):
             df_classe = pd.read_excel(os.path.join(folder_path, basename(filename)), header = 1)
             Classe = Classe.append(df_classe, ignore_index = False)
-        if ((FileExtension == ".xlsx") and ("liste" in FilePrefix)):
+        if ((FileExtension == ".xlsx") and ("listeVoe" in FilePrefix)):
             df_voeux = pd.read_excel(os.path.join(folder_path, basename(filename)))
             ListeVoeux = ListeVoeux.append(df_voeux, ignore_index = False)
-             
+
+ListeVoeux.to_excel("ListeV.xlsx", index=False)
 
 Classe.rename(columns={'login':'CODE_CANDIDAT'}, inplace = True)
 
@@ -43,48 +45,96 @@ Nbr_ecole = len(ListeEcole.index)
 
 ListeEcole.rename(columns={'Ecole':'Eco _cod'}, inplace = True)
 
-inner_join_df = tips[["CODE_CANDIDAT", "NOM", "PRENOM", "VOIE" ,"ETABLISSEMENT"]].merge(Classe[["CODE_CANDIDAT", "pays", "rang_classe"]], how="inner", on="CODE_CANDIDAT")
+inner_join_df = tips[["CODE_CANDIDAT", "NOM", "PRENOM", "VOIE", "LIBELLE_PAYS", "CODE_PAYS","ETABLISSEMENT"]].merge(Classe[["CODE_CANDIDAT","n_demi","total_avec_interclassement", "rang_classe"]], how="inner", on="CODE_CANDIDAT")
 
 Nbr_etudiants = len(inner_join_df.index)
 
-inner_join_df_2 = inner_join_df[["CODE_CANDIDAT", "NOM", "PRENOM", "VOIE" ,"ETABLISSEMENT", "pays", "rang_classe"]].merge(ListeVoeux[["CODE_CANDIDAT", "Voe _ord","Eco _cod"]], how="inner", on="CODE_CANDIDAT")
+inner_join_df_2 = inner_join_df[["CODE_CANDIDAT", "NOM", "PRENOM", "VOIE" ,"ETABLISSEMENT", "LIBELLE_PAYS", "CODE_PAYS","n_demi","total_avec_interclassement", "rang_classe"]].merge(ListeVoeux[["CODE_CANDIDAT", "Voe _ord","Eco _cod"]], how="inner", on="CODE_CANDIDAT")
 
-inner_join_df_3 = inner_join_df_2[["CODE_CANDIDAT", "NOM", "PRENOM", "VOIE" ,"ETABLISSEMENT", "pays", "rang_classe", "Voe _ord","Eco _cod"]].merge(ListeEcole[["Eco _cod", "Nom _ecole"]], how="inner", on="Eco _cod")
+inner_join_df_3 = inner_join_df_2[["CODE_CANDIDAT", "NOM", "PRENOM", "VOIE" ,"ETABLISSEMENT", "LIBELLE_PAYS", "CODE_PAYS","n_demi","total_avec_interclassement","rang_classe", "Voe _ord","Eco _cod"]].merge(ListeEcole[["Eco _cod", "Nom _ecole"]], how="inner", on="Eco _cod")
 
 inner_join_df_3.sort_values(by = ["rang_classe", "Voe _ord"], ascending = [True,True], inplace = True)
 
 
-EtuParEcole = ceil(Nbr_etudiants/Nbr_ecole)
+EtuParEcole = ceil(Nbr_etudiants/Nbr_ecole) #Ici 26 élèves par école
 
-EleveDejaRecu = []
 
 DicoEcolePlace = {}
 for i in ListeEcole["Nom _ecole"]:
     DicoEcolePlace[i]=0
 
 
-
-# candidati = inner_join_df_3[inner_join_df_3["CODE_CANDIDAT"] == 80511]
-# print(candidati["CODE_CANDIDAT"].index)
-# print(candidati["Nom _ecole"])
-
-# A = candidati["Nom _ecole"].iloc[1]
-# print(A)
-
-# print(DicoEcolePlace[A])
-
 for i in inner_join_df_3["CODE_CANDIDAT"].unique():
-    candidati = inner_join_df_3[inner_join_df_3["CODE_CANDIDAT"] == i]
+    candidati = inner_join_df_3[inner_join_df_3["CODE_CANDIDAT"] == i] #select where code_candidat = i
 
     for j in candidati["Nom _ecole"]:
         if DicoEcolePlace[j] <= EtuParEcole:
             inner_join_df_3.loc[candidati["CODE_CANDIDAT"].index,"Ecole intégrée"] = j
             DicoEcolePlace[j] +=1
             break
-    if (Le candidat n'a aucune école :):
-        Prenom = inner_join_df_3.loc[candidati["CODE_CANDIDAT"].index,"PRENOM"]
-        Nom = inner_join_df_3.loc[candidati["CODE_CANDIDAT"].index,"NOM"]
-        print(Prenom+" "+Nom+" n'intégre aucune école.\n")
+    # if (Le candidat n'a aucune école :):
+    #     Prenom = inner_join_df_3.loc[candidati["CODE_CANDIDAT"].index,"PRENOM"]
+    #     Nom = inner_join_df_3.loc[candidati["CODE_CANDIDAT"].index,"NOM"]
+    #     print(Prenom+" "+Nom+" n'intégre aucune école.\n")
 
 
-inner_join_df_3.to_excel("Innerjoin.xlsx", index=False)
+inner_join_df_3.to_excel("Integration.xlsx", index=False)
+
+#################################################################################################################
+#inner_join_df_3 = pd.read_excel("Innerjoin.xlsx")
+
+dico_pays = {}
+dico_pays["Gabon"] = "ga"
+dico_pays["Maroc"] = "ma"
+dico_pays["Tunisie"] = "tn"
+dico_pays["Japon"] = "jp"
+dico_pays["Côte D'Ivoire"] = "ci"
+dico_pays["Espagne"] = "es"
+dico_pays["Luxembourg"] = "lu"
+dico_pays["Canada"] = "ca"
+dico_pays["Mauritanie"] = "mr"
+dico_pays["Monaco"] = "mc"
+dico_pays["Pakistan"] = "pk"
+dico_pays["Grande-Bretagne"] = "gb"
+dico_pays["Allemagne"] = "de"
+dico_pays["Andorre"] = "ad"
+dico_pays["Liban"] = "lb"
+dico_pays["Sénégal"] = "sn"
+dico_pays["Etats-Unis"] = "us"
+dico_pays["Belgique"] = "be"
+dico_pays["Italie"] = "it"
+
+
+Listepays = []
+ListeEcole = ['LIBELLE_PAYS', 'country_id']
+Codepays = []
+
+for i in inner_join_df_3["LIBELLE_PAYS"].unique():
+    Listepays.append(i)
+
+for i in inner_join_df_3["Ecole intégrée"].unique():
+    ListeEcole.append(i)
+
+for row in Listepays:
+    Codepays.append(dico_pays[row])
+
+
+final = pd.DataFrame({
+     'LIBELLE_PAYS':
+     Listepays,
+     'country_id':
+     Codepays
+ }, index=Listepays, columns=ListeEcole)
+final = final.fillna(0) # Remplie le tableau avec des 0
+
+
+for i in inner_join_df_3["CODE_CANDIDAT"].unique():
+    candidati = inner_join_df_3[inner_join_df_3["CODE_CANDIDAT"] == i]
+    A = candidati["LIBELLE_PAYS"]
+    for a in A.unique():
+        for j in candidati["Ecole intégrée"].unique():
+            final.loc[a,j] +=1
+            break
+
+
+final.to_csv("Admis_geo.csv", index=False)
